49c051b2ec6e03a039675de0d0210406f048dda5
Manizha-khorram <108601629+Manizha-khorram@users.noreply.github.com>
2022-09-14 01:55:01 -0400

    final-Project

--- js/index.js
+++ js/index.js
@@ -1,15 +1,23 @@
 
-//creating this year date beside my name 
+//togleBUTTON
+const toggleButton = document.getElementById('menu');
+const naviList = document.getElementById('navi-list');
+  toggleButton.addEventListener('click',(e)=>{
+    naviList.classList.toggle('active');
+    console.log("e");
+  })
+
+
+//creating this year date beside my name in the footer section 
 let today= new Date();
 let thisYear= today.getFullYear();
-
 let footer=document.querySelector('footer');
 let copyright=document.createElement('p');
 copyright.innerHTML=`Manizha Khorram ${thisYear}`;
 footer.appendChild(copyright);
 
 //My skills list
-let skills=['MS Office', 'Adobe Illustrator','Programing languages (HTML, CSS, JavaScript)'];
+let skills=['HTML', 'CSS','JavaScript'];
 let skillsSection=document.querySelector('#skills');
 let skillsList=skillsSection.querySelector('ul');
 for(let i=0; i<skills.length; i++){
@@ -37,10 +45,13 @@ messageForm.item(0).addEventListener("submit",(event) => {
     removeButton.type='button';
     editButton.innerText='edit';
     editButton.type='button';
-    messageList.addEventListener('click',(e)=>{
+    newMessage.addEventListener('click',(e)=>{
         if(e.target.tagName==="BUTTON"){
         const button=e.target;
         let entry=removeButton.parentNode;
+        console.log(e.target);
+        console.log("here is entry",entry);
+        console.log("here is the target",e.target.parentNode);
         if(button.innerText==='remove'){
             entry.remove();
         }else if(button.innerText==='edit'){
@@ -50,16 +61,16 @@ messageForm.item(0).addEventListener("submit",(event) => {
             input.value=span.innerText;
             newMessage.insertBefore(input,span);
             newMessage.removeChild(span);
-            button.innerText='save';
+            button.innerText ='save';
 
-        }else if(button.innerText==='save'){
-            const input= newMessage.childNodes[2];
-            const span=document.createElement('span');
+        }else if(button.innerText === 'save'){
+            const input = newMessage.childNodes[2];
+            const span = document.createElement('span');
             span.type='text';
-            span.innerText=input.value;
+            span.innerText = input.value;
             newMessage.insertBefore(span,input);
             newMessage.removeChild(input);
-            button.innerText='edit';
+            button.innerText = 'edit';
         }
 }})
       messageList.appendChild(newMessage);
@@ -67,3 +78,22 @@ messageForm.item(0).addEventListener("submit",(event) => {
       newMessage.appendChild(editButton);
       messageForm.item(0).reset();
 })
+
+
+
+
+    
+fetch('https://api.github.com/users/Manizha-khorram/repos')
+ .then(response => response.json())
+ .then( data => {
+      console.log(data);
+     let projectSection = document.getElementById('projects');
+     let projectList = projectSection.querySelector('ul');
+     for(let i = 0; i < data.length; i++){
+         let project = document.createElement('li');
+         project.innerHTML =`<a href="${data[i].html_url}"> ${data[i].name}</a>`
+         projectList.appendChild(project);
+    
+             } }).catch(error => console.log('Looks like there is an error!!',error))
+ 
+ 
